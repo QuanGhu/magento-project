f686f26afb55e95b2d45872811b6f7c85bbd3fe8
Ari Putra <ariteknologi@gmail.com>
2020-01-18 20:28:48 +0700

    add midtrans module

---
+++ app/code/Midtrans/Snap/Block/Form/Snap.php
@@ -0,0 +1,15 @@
+<?php
+namespace Midtrans\Snap\Block\Form;
+
+class Snap extends \Magento\Payment\Block\Form
+{
+    /**
+     * Instructions text
+     *
+     * @var string
+     */
+    protected $_instructions;
+
+    protected $_template = 'form/snap.phtml';
+
+}
